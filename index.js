f1555cad79194c56f937c884db7243734dcd3cab
Nand Kishor <nandkk07@gmail.com>
2021-05-28 13:55:19 +0530

    setup

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const getVideos = require('./services/notion')
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+app.get('/videos', async (req,res) => {
+    const videos = await getVideos()
+    res.json(videos)
+})
+
+app.listen(PORT, console.log(`Server started on ${PORT}`))
